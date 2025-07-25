107a1e4ece2d49737fdcecdf38273324d40b2fe9
Owner <Owner@DESKTOP-IG6QON0>
2025-07-25 14:43:37 +0900

    검색 기능 추가(지수님꺼)

---
+++ app/src/main/java/com/example/swu_guru2_android_app/SearchMain.kt
@@ -0,0 +1,21 @@
+package com.example.swu_guru2_android_app
+
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SearchMain {
+    // 검색 기능 (TextWatcher)
+    binding.searchEditText.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+            searchJob?.cancel()
+            searchJob = CoroutineScope(Dispatchers.Main).launch {
+                delay(300)
+                val query = s?.toString()?.lowercase()?.trim() ?: ""
+                val filtered = originalList.filter { ex ->
+}
