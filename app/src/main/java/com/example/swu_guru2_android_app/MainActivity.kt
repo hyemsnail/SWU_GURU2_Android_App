9023c803b679c57e27029f47f5ec8572fd5f1664
JisooLee <jksy010702010702@gmail.com>
2025-07-28 00:27:27 +0900

    메인 화면

--- app/src/main/java/com/example/swu_guru2_android_app/MainActivity.kt
+++ app/src/main/java/com/example/swu_guru2_android_app/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.swu_guru2_android_app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +10,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val goSelectExerciseBtn = findViewById<Button>(R.id.btn_go_select_exercise)
+        goSelectExerciseBtn.setOnClickListener {
+            val intent = Intent(this, SelectExercise::class.java)
+            startActivity(intent)
+        }
+
+        val goScheduleBtn = findViewById<Button>(R.id.btn_go_select_exercise2)
+        goScheduleBtn.setOnClickListener {
+            val intent = Intent(this, ViewScheduleActivity::class.java)  // ✅ 운동 스케줄 화면으로 이동
+            startActivity(intent)
         }
     }
 }
